851a1ad721b2b282a25be08a0c57fedaf3499a11
Luiza Sá <luizasa.dev@gmail.com>
2026-01-12 19:19:57 -0300

    fetch animais refatorado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,23 +1,8 @@
 import AnimaNumeros from "./anima-numeros.js";
 
-export default function initFetchAnimais() {
-  async function fetchAnimais(url) {
-    try {
-      const animaisResponse = await fetch(url);
-      const animaisJSON = await animaisResponse.json();
-      const numerosGrid = document.querySelector('.numeros-grid');
-      animaisJSON.forEach((item) => {
-        const divAnimal = createAnimal(item);
-        numerosGrid.appendChild(divAnimal);
-      });
-      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
-      animaNumeros.init();
-    }
-    catch (erro) {
-      console.log(erro);
-    }
-  }
-
+export default function fetchAnimais(url, target) {
+  // Cria a div contendo informações
+  // com o total de animais
   function createAnimal(animal) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
@@ -25,11 +10,34 @@ export default function initFetchAnimais() {
     return div;
   }
 
-  fetchAnimais('./animaisapi.json');
-}
+  // Preenche cada animal no DOM
+  const numerosGrid = document.querySelector(target);
+  function preencherAnimais(animal) {
+    const divAnimal = createAnimal(animal);
+    numerosGrid.appendChild(divAnimal);
+  }
 
-// EXPLICAÇÃO:
-// primeira coisa sempre é criar a função que vai fazer o uso assíncrono! aqui utilizamos a async function, mas poderia ser utilizado o fetch direto com o .then como havíamos visto anteriormente (async/await é a forma mais 'moderna' de fazer requisições assíncronas, mas não é suportado por browsers mais antigos).
-// depois criamos a função que vai criar os elementos html com base na api (função createAnimal). criamos uma div e definimos seu conteudo html, reescrevendo da mesma forma que a estrutura que utilizamos no html, mas substituindo os valores pelos valores fornecidos pela api. retornamos a div para poder utilizar ela na nossa função principal.
-// voltamos para a função principal para adicionar o elemento criado ao html do site. selecionamos a div que contém os animais e números (.numeros-grid) e, para inserir a div criada nele, utilizamos o appendChild (inserir um nó filho a um pai, ou seja, inserir a div criada (JS: divAnimal) dentro da div pai (JS: numerosGrid)))
-// por último importamos a função initAnimaNumeros() aqui, removendo do script principal, e adicionamos/ativamos ela após o fetch acontecer - isso pq mantendo ela no script principal pode dar problema do JS tentar executar a função initAnimaNumeros() antes da fetch desta função aqui acontecer e aí a animação não funcionar.
+  // Anima os números de cada animal
+  function animaAnimaisNumeros() {
+    const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+    animaNumeros.init();
+  }
+  // Puxa os anuimais através de um arquivo json
+  // e cria cada animal utilizando createAnimal
+  async function criarAnimais() {
+    try {
+      // Fetch, espera resposta e transforma em JSON
+      const animaisResponse = await fetch(url);
+      const animaisJSON = await animaisResponse.json();
+      // Após a tranformação de JSON ativa as funções
+      // para preencher e animar os números
+      animaisJSON.forEach(animal => preencherAnimais(animal));
+      animaAnimaisNumeros();
+    }
+    catch (erro) {
+      console.log(erro);
+    }
+  }
+
+  return criarAnimais();
+}
