768212ad9c1daada376fe936e25c0cb83ab891b0
Luiza Sá <luizasa.dev@gmail.com>
2026-01-13 18:51:35 -0300

    fetch bitcoin refatorado

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
 
 
@@ -26,11 +26,11 @@ modal.init();
 const tooltip = new ToolTip('[data-tooltip]');
 tooltip.init();
 
-
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
-
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
 initAnimacaoScroll();
+
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
+
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
