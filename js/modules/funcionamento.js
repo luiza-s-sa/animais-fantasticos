577c91ffece64655eea481953a0cd20a3cb3965e
Luiza Sá <luizasa.dev@gmail.com>
2026-01-25 10:02:19 -0300

    funcionamento transformado em classe

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,41 +1,39 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector('[data-semana]');
-  const diasSemana = funcionamento.dataset.semana.split(',').map(Number); // transormar a split em uma array, e map (map itera por cada elemento do array) para transformar as strings em números
-  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
-
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horarioAgora = dataAgora.getHours();
-
-  const abertoDia = diasSemana.indexOf(diaAgora) !== -1;
-  const abertoHora = (horarioSemana[0] <= horarioAgora && horarioSemana[1] > horarioAgora);
-
-  if (abertoDia && abertoHora) {
-    funcionamento.classList.add('aberto');
+export default class Funcionamento {
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
   }
+
+  dadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
+    this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
+  }
+
+  dadosAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+    this.horarioAgora = this.dataAgora.getUTCHours() - 3;
+  }
+
+  estaAberto() {
+    const abertoDia = this.diasSemana.indexOf(this.diaAgora) !== -1;
+    const abertoHora = (this.horarioSemana[0] <= this.horarioAgora && this.horarioSemana[1] > this.horarioAgora);
+    return abertoDia && abertoHora;
+  }
+
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
+    }
+    return this;
+  }
+
 }
-// const agora = new Date();
-// console.log(agora)
-// console.log(agora.getDate())
-// console.log(agora.getDay()) // de 0 a 6, começando o 0 no domingo
-
-// const natal = new Date('Dec 24 2025 23:59');
-// const anoNovo = new Date('Dec 31 2025 23:59');
-// console.log(natal)
-// console.log(anoNovo)
-
-// function transformarMiliSegundosEmDias(tempo) {
-//   return tempo / (24 * 60 * 60 * 1000);
-// }
-
-// const diasAgora = transformarMiliSegundosEmDias(agora.getTime())
-// const diasNatal = transformarMiliSegundosEmDias(natal.getTime())
-// const diasAnoNovo = transformarMiliSegundosEmDias(anoNovo.getTime())
-
-// console.log(agora.getTime())
-// console.log(transformarMiliSegundosEmDias(agora.getTime()))
-// console.log(natal.getTime())
-
-// console.log(diasNatal - diasAgora)
-// console.log(`Faltam ${Math.floor(diasNatal - diasAgora)} dias para o Natal`)
-// console.log(`Faltam ${Math.floor(diasAnoNovo - diasAgora)} dias para o Ano Novo`)
